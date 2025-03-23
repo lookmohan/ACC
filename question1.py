8276ed9028e24dfcaeb75cb262031d137cddc116
Mohanraj.R <142210548+lookmohan@users.noreply.github.com>
2025-03-23 19:17:56 +0530

    Create question1.py

---
+++ question1.py
@@ -0,0 +1,20 @@
+
+n = int(input())
+prices = [int(input()) for _ in range(n)]
+
+def max_profit(prices):
+    if len(prices) < 2:
+        return 0
+    
+    left = 0  
+    right = 1  
+    max_profit = 0
+    while right < len(prices):
+        if prices[right] > prices[left]:
+            profit = prices[right] - prices[left]
+            max_profit = max(max_profit, profit)
+        else:
+            left = right
+        right += 1
+    return max_profit
+print(max_profit(prices))
